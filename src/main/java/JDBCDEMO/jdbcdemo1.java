9e2ff5a2c990b1364c2ff134e6b2585cf47f34bc
kaiqi <zhang20170730@gmail.com>
2023-04-15 15:43:15 +0800

    jdbcdemo 基本操作

--- src/main/java/JDBCDEMO/jdbcdemo1.java
+++ src/main/java/JDBCDEMO/jdbcdemo1.java
@@ -4,12 +4,12 @@ import java.sql.*;
 
 public class jdbcdemo1 {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.jdbc.Driver");
+//        Class.forName("com.mysql.jdbc.Driver");  mysql5之后不需要注册驱动
         Connection conn = DriverManager.getConnection("jdbc:mysql://192.168.0.201:3306/test", "root", "root");
         Statement statement = conn.createStatement();
         ResultSet resultSet = statement.executeQuery("SELECT * FROM account");
         while (resultSet.next()){
-            System.out.println(resultSet.getString("name"));
+            System.out.println(resultSet.getString("name") + resultSet.getString("money"));
         }
 
         statement.close();
