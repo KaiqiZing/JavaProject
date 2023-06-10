782a7c0e59ee8920e9f2a044957be42ce1aeb4fb
kaiqi <zhang20170730@gmail.com>
2023-06-10 10:07:23 +0800

    netty

--- src/main/java/com/mogodbdemo/mogodbdemo1.java
+++ src/main/java/com/mogodbdemo/mogodbdemo1.java
@@ -3,6 +3,7 @@ package com.mogodbdemo;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
@@ -10,29 +11,39 @@ public class mogodbdemo1 {
 
     public static void main(String[] args) {
         try {
-            // 连接到本地 MongoDB 服务器
+            // ??????±??? MongoDB ·????÷
             MongoClient mongoClient = new MongoClient("192.168.0.106", 27017);
 
-            // 连接到数据库
+            // ????????????
             MongoDatabase database = mongoClient.getDatabase("cx_mcs");
 
-            // 获取集合
-            MongoCollection<Document> collection = database.getCollection("mycollection");
-            System.out.println(collection.toString());
+            // ????????
+            MongoCollection<Document> collection = database.getCollection("cms_page");
+            System.out.println(collection);
 
-            // 插入文档
-            Document document = new Document("name", "John")
-                    .append("age", 30)
-                    .append("city", "New York");
-            collection.insertOne(document);
+            // ?????é??
+            MongoCursor<Document> cursor = collection.find().iterator();
+            System.out.println(cursor);
 
-            // 更新文档
-            collection.updateOne(new Document("name", "John"), new Document("$set", new Document("age", 35)));
+            // ±é?ú?á????
+            while (cursor.hasNext()) {
+                Document doc = cursor.next();
+                System.out.println(doc.toJson());
+            }
 
-            // 删除文档
-            collection.deleteOne(new Document("name", "John"));
+//            // ????????
+//            Document document = new Document("name", "John")
+//                    .append("age", 30)
+//                    .append("city", "New York");
+//            collection.insertOne(document);
+//
+//            // ?ü??????
+//            collection.updateOne(new Document("name", "John"), new Document("$set", new Document("age", 35)));
+//
+//            // ????????
+//            collection.deleteOne(new Document("name", "John"));
 
-            // 关闭连接
+            // ??±?????
             mongoClient.close();
 
         } catch (MongoException e) {
